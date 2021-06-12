5ce25b486137bcc6f1ba9017d18cde5824ddbb33
Abhishek <ak4290104@gmail.com>
2021-06-12 22:14:26 +0800

    Controllers and Migrations are done

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,28 @@
+using Persistent;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Domain;
+using System.Collections.Generic;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ActivitiesController: BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context){
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities(){
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id){
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
---
+++ Persistent/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistent
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options): base(options){}
+         public DbSet<Activity> Activities { get; set; }
+    }
+}
